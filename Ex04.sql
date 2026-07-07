3f584f7349b8b1727cbf90c308168d1464a49ca9
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-07 12:30:47 +0700

    Ex04

---
+++ Ex04.sql
@@ -0,0 +1,9 @@
+-- Bài tập:  Tạo bảng có DEFAULT và CHECK
+CREATE TABLE users (
+    user_id VARCHAR(10) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    status VARCHAR(10) DEFAULT 'ACTIVE',
+    CHECK (status IN ('ACTIVE', 'INACTIVE'))
+);
+DESC users;
