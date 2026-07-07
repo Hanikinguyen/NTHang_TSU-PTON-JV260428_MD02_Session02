c4ef61881a1185d287788f4e802223689850d9dd
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-07 12:48:49 +0700

    Ex06

---
+++ Ex06.sql
@@ -0,0 +1,30 @@
+-- Bài tập: Quản lý đơn hàng - sản phẩm (advanced)
+CREATE TABLE orders (
+    order_id VARCHAR(10) PRIMARY KEY,
+    order_date DATE NOT NULL,
+    status VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE products_order (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR(10) NOT NULL,
+    product_id VARCHAR(10) NOT NULL,
+    quantity INT NOT NULL,
+
+    PRIMARY KEY (order_id, product_id),
+
+    FOREIGN KEY (order_id)
+        REFERENCES orders(order_id),
+
+    FOREIGN KEY (product_id)
+        REFERENCES products_order(product_id)
+);
+SHOW TABLES;
+DESC orders;
+DESC products_order;
+DESC order_items;
